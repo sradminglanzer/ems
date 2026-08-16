f18a3b06fa8b91a08369a64284389d5096088979
Admin Glanzer <admin@srglanzsoftware.com>
2026-08-16 09:09:36 +0530

    production build ready

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.srgs.ems"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 4
+        versionName = "1.0.4"
 
         vectorDrawables { useSupportLibrary = true }
     }
